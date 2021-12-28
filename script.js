afa2c219d1c9c275ad19fcee81ac32abad9cdc57
Vasile Midrigan <111rigan111@gmail.com>
2021-12-28 00:02:08 +0000

    desktop version implemented

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 "use strict";
 // Variables
-let input = document.getElementById("email-address");
-const submitButton = document.querySelector(".submit-btn");
+const input = document.getElementById("email-address");
+const errorMessage = document.querySelector(".error-message");
+const exclamationMark = document.querySelector(".exclamation-mark");
 
-// Clear input field when click on them
-input.addEventListener("click", function () {
-  input.placeholder = "";
+// Prevent default behaviour of the error message tooltip
+input.addEventListener(
+  "invalid",
+  function (el) {
+    el.preventDefault();
+  },
+  true
+);
+
+// ---- If the email format isn't correct ----
+// 1. Adding a red 1px border to the input field
+// 2. Showing the exclamation mark and the error message
+input.addEventListener("invalid", function () {
+  input.style.border = "1px solid red";
+  exclamationMark.classList.remove("hidden");
+  errorMessage.classList.remove("hidden");
 });
